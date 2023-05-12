736aa32c6074e45beec071adb99198a954073cee
r3c4 <vinicius.rech.dev@gmail.com>
2023-05-12 01:16:24 -0300

    chore: implemented favicon and cleaned dump code.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,10 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head>
+        <title>Dashboard</title>
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+    </Head>
 
     <AuthenticatedLayout>
         <template #header>
