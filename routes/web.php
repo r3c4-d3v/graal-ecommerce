20eefbb8f0e36110c07c238c3c9b0b5f54e4a6db
r3c4 <vinicius.rech.dev@gmail.com>
2023-05-16 20:19:33 -0300

    style: routes style has been improved.

--- routes/web.php
+++ routes/web.php
@@ -8,48 +8,50 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-//Route::get('/', function () {
-//    return Inertia::render('Customer/Home', [
-//        'canLogin' => Route::has('login'),
-//        'canRegister' => Route::has('register'),
-//        'laravelVersion' => Application::VERSION,
-//        'phpVersion' => PHP_VERSION,
-//    ]);
-//});
+const ADMIN_PREFIX = '/admin';
+const PROFILE = '/profile';
+const PRODUCT = '/product';
+const CATEGORY = '/category';
 
+# Public Routes
+Route::get('/login', [LoginController::class, 'index'])
+    ->name('login');
+
+# Admin Routes
 Route::middleware(['admin.auth', 'verified'])->group(function () {
-    # Admin
-    Route::prefix('/admin')->group(function () {
+    Route::prefix(ADMIN_PREFIX)->group(function () {
+
+        # Dashboard
         Route::get('/dashboard', function () {
             return Inertia::render('Admin/Dashboard');
         })->name('admin.dashboard');
 
         # Profile edit
-        Route::get('/profile', [ProfileController::class, 'edit'])
+        Route::get(PROFILE, [ProfileController::class, 'edit'])
             ->name('admin.profile.edit');
 
         # Profile update
-        Route::patch('/profile', [ProfileController::class, 'update'])
+        Route::patch(PROFILE, [ProfileController::class, 'update'])
             ->name('admin.profile.update');
 
         # Profile delete
-        Route::delete('/profile', [ProfileController::class, 'destroy'])
+        Route::delete(PROFILE, [ProfileController::class, 'destroy'])
             ->name('admin.profile.destroy');
 
         # Product index
-        Route::get('/product', [ProductController::class, 'index'])
+        Route::get(PRODUCT, [ProductController::class, 'index'])
             ->name('admin.product.index');
 
         # Product store
-        Route::post('/product', [ProductController::class, 'store'])
+        Route::post(PRODUCT, [ProductController::class, 'store'])
             ->name('admin.product.store');
 
         # Category
-        Route::get('/category', [ProductCategoryController::class, 'index'])
+        Route::get(CATEGORY, [ProductCategoryController::class, 'index'])
             ->name('admin.category.index');
 
         # Category store
-        Route::post('/category', [ProductCategoryController::class, 'store'])
+        Route::post(CATEGORY, [ProductCategoryController::class, 'store'])
             ->name('admin.category.store');
     });
 });
@@ -58,7 +60,4 @@ Route::middleware(['admin.auth', 'verified'])->group(function () {
 Route::get('/', [CustomerController::class, 'index'])
     ->name('customer.index');
 
-Route::get('/login', [LoginController::class, 'index'])
-    ->name('login');
-
-require __DIR__ . '/auth.php';
+require_once __DIR__ . '/auth.php';
